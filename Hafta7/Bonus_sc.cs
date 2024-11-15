eb3b54e2b5fad822c1b7e7babc004de4fcc6b435
MustafaAykut77 <168454893+MustafaAykut77@users.noreply.github.com>
2024-11-15 20:02:31 +0300

    Add files via upload

---
+++ Hafta7/Bonus_sc.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonus_sc : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 3.0f;
+
+    [SerializeField]
+    int bonusId;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0,-1,0) * speed * Time.deltaTime);
+        if(transform.position.y < -5.8f){
+            Destroy(this.gameObject);
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            Player_sc player_sc = other.transform.GetComponent<Player_sc>();
+            if(player_sc != null){
+                switch(bonusId){
+                    case 0:
+                        player_sc.ActivateTripleShot();
+                        break;
+                    case 1:
+                        player_sc.ActivateSpeedBonus();
+                        break;
+                    case 2:
+                        Debug.Log("Shield Bonus");
+                        break;
+                    default:
+                        Debug.Log("Default value in switch case");
+                        break;
+                }
+                
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Hafta7/SpawnManager_sc.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager_sc : MonoBehaviour
+{  
+    [SerializeField]
+    GameObject enemyPrefab;
+
+    [SerializeField]
+    GameObject enemyContainer;
+
+    [SerializeField]
+    GameObject[] BonusPrefabs;
+
+    bool stopSpawning = false;
+
+    IEnumerator SpawnEnemyRoutine(){
+        while(stopSpawning == false){
+            Vector3 position = new Vector3(Random.Range(-9.4f, 9.4f), 7.4f, 0);
+            GameObject new_enemy = Instantiate(enemyPrefab, position, Quaternion.identity);
+            new_enemy.transform.parent = enemyContainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator SpawnBonusRoutine(){
+        while(stopSpawning == false){
+            Vector3 position = new Vector3(Random.Range(-9.4f, 9.4f), 7.4f, 0);
+            Instantiate(BonusPrefabs[Random.Range(0,2)], position, Quaternion.identity);
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+    public void OnPlayerDeath(){
+        stopSpawning = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnBonusRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
